34f99392b1ffbbcf9a4e8be11553f51f34ac63a6
yifatdzigan <y.dzigan@esciencecenter.nl>
2019-02-19 11:31:35 +0100

    Add setup.py
    Initial code dump

---
+++ setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="pyERA5",
+    version="0.0.1",
+    author="Jerom Aerts",
+    author_email="J.P.M.Aerts@tudelft.nl",
+    description=("A python library to download ERA5 "
+                 "from the Climate Data Store https://climate.copernicus.eu/."),
+    license="Apache 2.0",
+    keywords="ERA5",
+    url="https://github.com/ewatercycle/pyERA5",
+    packages=['pyERA5'],
+    include_package_data=False,    # include everything in source control
+    scripts=['pyERA5/scripts/pyEAR5'],
+    long_description=read('README.rst'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    install_requires=['requests', 'configargparse'],
+)
