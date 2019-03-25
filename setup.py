30502c803b803dc266025c843817b99670925e7b
Ronald van Haren <r.vanharen@esciencecenter.nl>
2019-03-25 17:07:41 +0100

    cleanup; add additional command line options

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
-import os
+"""Hydro ERA5 download package."""
 from setuptools import setup
 
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
+with open('README.rst') as file:
+    README = file.read()
 
 setup(
     name="pyERA5",
     version="0.0.1",
-    author="Jerom Aerts", "Yifat Dzigan" 
-    author_email="J.P.M.Aerts@tudelft.nl", "y.dzigan@esciencecenter.nl"
+    author="Jerom Aerts, Yifat Dzigan, Ronald van Haren",
+    author_email=("J.P.M.Aerts@tudelft.nl, y.dzigan@esciencecenter.nl, "
+                  "r.vanharen@esciencecenter.nl"),
     description=("A python library to download ERA5 "
-                 "from the Copernicus Climate Data Store https://climate.copernicus.eu/."),
+                 "from the Copernicus Climate Data Store "
+                 "https://climate.copernicus.eu/."),
     license="Apache 2.0",
     keywords="ERA5",
     url="https://github.com/ewatercycle/pyERA5",
-    packages=['pyERA5'],
-    include_package_data=False,    # include everything in source control
+    packages=['pyera5'],
+    include_package_data=True,    # include everything in source control
+    package_data={'pyera5': ['cartesius/*']},
     scripts=['pyera5/scripts/pyera5'],
-    long_description=read('README.rst'),
+    long_description=README,
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Software Development :: Libraries :: Python Modules",
