f962a6ecce25720a7f535561b83a300a737e47c9
Ronald van Haren <r.vanharen@esciencecenter.nl>
2019-04-02 21:39:31 +0200

    allow splitting by variables, change default output naming

--- era5cli/fetch.py
+++ era5cli/fetch.py
@@ -3,14 +3,13 @@
 import cdsapi
 # from pathos.multiprocessing import ProcessPool as Pool
 from pathos.threading import ThreadPool as Pool
-import os
 
 
 class Fetch:
     """Fetch ERA5 data using cdsapi."""
 
     def __init__(self, years, months, days, hours, variables, outputformat,
-                 outputfile, split=None, threads=None):
+                 outputprefix, split=None, threads=None):
         """Initialization of Fetch class."""
         self.months = months
         self.days = days
@@ -18,36 +17,90 @@ class Fetch:
         self.variables = variables
         self.outputformat = outputformat
         self.years = years
-        self.outputfile = outputfile
+        self.outputprefix = outputprefix
         self.threads = threads
+        self.split = split
 
-        if (split == 'year'):
+        # define extension output filename
+        self.extension()
+
+    def fetch(self):
+        """Split calls and fetch results."""
+        # fetch files
+        if all([x in self.split for x in ['variable', 'year']]):
+            # split by variable
+            self.split_variable_yr()
+
+        elif 'year' in self.split:
+            # split by year
             self.split_yr()
-        elif not split:
-            self.fetch(self.years, self.outputfile)
 
-    def split_yr(self):
-        """Fetch variable split by year."""
-        (fname, ext) = os.path.splitext(self.outputfile)
-        # generate output filenames
-        outputfiles = ["{}_{}{}".format(fname, yr, ext) for yr in self.years]
+        elif 'variable' in self.split:
+            self.split_variable()
+
+        elif not self.split:
+            self.outputfile = '{}.{}'.format(self.outputprefix, self.ext)
+            self.getdata(self.variables, self.years, self.outputfile)
+
+    def extension(self):
+        """Set filename extension."""
+        if (self.outputformat.lower() == 'netcdf'):
+            self.ext = "nc"
+        elif (self.outputformat.ower() == 'grib'):
+            self.ext = 'grb'
+        else:
+            raise Exception('Unknown outputformat: {}'.format(
+                            self.outputformat))
+
+    def split_variable(self):
+        """Split by variable."""
+        outputfiles = ["{}_{}.{}".format(self.outputprefix, var, self.ext)
+                       for var in self.variables]
+        years = len(outputfiles) * [self.years]
         if not self.threads:
             pool = Pool()
         else:
-            if (self.threads > len(self.years)):
-                self.threads = len(self.years)
-            pool = Pool(nodes=self.threads)
-        pool.map(self.fetch, self.years, outputfiles)
+            pool = Pool(nodes=max(self.threads, len(outputfiles)))
+        pool.map(self.getdata, self.variables, years, outputfiles)
 
-    def fetch(self, years, outputfile):
+    def split_yr(self):
+        """Fetch variable split by year."""
+        # generate output filenames
+        outputfiles = ["{}_{}.{}".format(self.outputprefix, yr, self.ext)
+                       for yr in self.years]
+        variables = len(outputfiles) * [self.variables]
+        if not self.threads:
+            pool = Pool()
+        else:
+            pool = Pool(nodes=max(self.threads, len(outputfiles)))
+        pool.map(self.getdata, variables, self.years, outputfiles)
+
+    def split_variable_yr(self):
+        """Fetch variable split by variable and year."""
+        outputfiles = []
+        years = []
+        variables = []
+        for var in self.variables:
+            outputfiles += ["{}_{}_{}.{}".format(self.outputprefix, var, yr,
+                                                 self.ext) for yr in
+                            self.years]
+            years += [yr for yr in self.years]
+            variables += len(outputfiles) * [var]
+        if not self.threads:
+            pool = Pool()
+        else:
+            pool = Pool(nodes=max(self.threads, len(outputfiles)))
+        pool.map(self.getdata, variables, years, outputfiles)
+
+    def getdata(self, variables, years, outputfile):
         """Fetch variables using cds api call."""
         c = cdsapi.Client()
         c.retrieve('reanalysis-era5-single-levels',
-                   {'variable': self.variables,
+                   {'variable': variables,
                     'product_type': 'reanalysis',
-                    'year': self.years,
+                    'year': years,
                     'month': self.months,
                     'day': self.days,
                     'time': self.hours,
                     'format': self.outputformat},
-                   self.outputfile)
+                   outputfile)
