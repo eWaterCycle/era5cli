91e12715a21cec733d26a53c43569c7af991f381
Ronald van Haren <r.vanharen@esciencecenter.nl>
2019-06-17 16:16:52 +0200

    flake8

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,12 +31,10 @@ release = '1.0.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-'sphinx.ext.autodoc',
-'sphinx.ext.coverage',
-'sphinx.ext.mathjax',
-'sphinx.ext.napoleon'
-]
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.coverage',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -58,4 +56,3 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
