424807cbb789c2d9d77913d8fc995e3d9f46f67e
Ronald van Haren <r.vanharen@esciencecenter.nl>
2019-06-20 17:52:47 +0000

    add more tests for argument parser (#7)
    * add more tests for argument parser
    
    * flake8

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,8 @@
 """Tests for era5cli utility functios."""
 
 import era5cli.cli as cli
+import unittest.mock as mock
+import pytest
 
 
 def test_str2bool():
@@ -31,3 +33,42 @@ def test_parse_args():
     assert args.statistics
     assert not args.threads
     assert args.variables == ['total_precipitation']
+
+
+@mock.patch("era5cli.fetch.Fetch", autospec=True)
+def test_main_fetch(fetch):
+    """Test if Fetch part of main completes without error."""
+    argv = ['hourly', '--startyear', '2008', '--ensemble', 'false',
+            '--variables', 'total_precipitation', '--statistics', 'true',
+            '--split', 'true', '--endyear', '2008', '--ensemble', 'true']
+    args = cli._parse_args(argv)
+    assert cli._execute(args)
+
+    # should give an AssertionError if endyear is before startyear
+    argv = ['hourly', '--startyear', '2008', '--ensemble', 'false',
+            '--variables', 'total_precipitation', '--statistics', 'true',
+            '--split', 'true', '--endyear', '2007', '--ensemble', 'true']
+    args = cli._parse_args(argv)
+    with pytest.raises(AssertionError):
+        assert cli._execute(args)
+
+    # monthly call without endyear
+    argv = ['monthly', '--startyear', '2008', '--ensemble', 'false',
+            '--variables', 'total_precipitation', '--synoptic', 'true',
+            '--split', 'true', '--ensemble', 'true']
+    args = cli._parse_args(argv)
+    cli._execute(args)
+
+
+@mock.patch("era5cli.info.Info", autospec=True)
+def test_main_info(info):
+    """Test if Info part of main completes without error."""
+    info.return_value.infotype = 'list'
+    argv = ['info', 'levels']
+    args = cli._parse_args(argv)
+    cli._execute(args)
+
+    info.return_value.infotype = 'total_precipitation'
+    argv = ['info', 'total_precipitation']
+    args = cli._parse_args(argv)
+    cli._execute(args)
