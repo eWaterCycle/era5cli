067a88d9e7706545831c36ba51caf2d4830a2fea
Ronald van Haren <r.vanharen@esciencecenter.nl>
2019-06-22 20:27:48 +0000

    Release cleanup (#15)
    * update version number for release
    
    * add zenodo integration file

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,6 +1,6 @@
 Changelog
 *********
 
-1.0.0 (2019-06-17)
+1.0.0rc1 (2019-06-22)
 ==================
-* First release of era5cli. A commandline utility to download ERA-5 data using cdsapi.
+* First Release Candidate for the stable 1.0.0 era5cli release: A commandline utility to download ERA-5 data using cdsapi.
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     package_data={'era5cli': ['cartesius/*']},
     long_description=README,
     classifiers=[
-        "Development Status :: 4 - Beta",
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Operating System :: MacOS :: MacOS X',
